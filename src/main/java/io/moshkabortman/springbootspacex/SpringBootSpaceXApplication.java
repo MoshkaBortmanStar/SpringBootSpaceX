fa865467b00187d281437001508c3272eca368eb
Mikhail Butorin <mikhail.butorin@axenix.pro>
2024-04-22 16:21:47 +0500

    Initial commit

---
+++ src/main/java/io/moshkabortman/springbootspacex/SpringBootSpaceXApplication.java
@@ -0,0 +1,13 @@
+package io.moshkabortman.springbootspacex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpaceXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpaceXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/moshkabortman/springbootspacex/SpringBootSpaceXApplicationTests.java
@@ -0,0 +1,13 @@
+package io.moshkabortman.springbootspacex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpaceXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
